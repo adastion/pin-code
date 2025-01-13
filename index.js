ea1677038f2232b9b9691677fa5b6f8e4c7a2ad8
Andrey Sery <andrey.72006@gmail.com>
2025-01-13 03:15:41 +0300

    deleted (type="module")

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-import { getPinCodeOptions } from "./src/features/getOptions.js"
+const POINTS = {
+  1: [1, 2, 4],
+  2: [1, 2, 3, 5],
+  3: [2, 3, 6],
+  4: [1, 4, 5, 7],
+  5: [2, 4, 5, 6, 8],
+  6: [3, 5, 6, 9],
+  7: [4, 7, 8],
+  8: [5, 7, 8, 9],
+  9: [6, 8, 9],
+  0: [0, 8],
+}
 
 const resultElement = document.querySelector(".result-block")
 const resetBtnElement = document.querySelector(".result-block__btn")
@@ -20,3 +31,32 @@ resetBtnElement.addEventListener("click", () => {
   result.innerHTML = ""
   resultElement.append(result)
 })
+
+function getPinCodeOptions(pinCode) {
+  let arrayOfCharacters
+  const result = []
+
+  if (typeof pinCode !== "string") {
+    arrayOfCharacters = String(pinCode).split("")
+  } else {
+    arrayOfCharacters = pinCode.split("")
+  }
+
+  if (typeof pinCode !== "string" && typeof pinCode !== "number" && isNaN(pinCode)) {
+    return console.log("Ошибка: Не допустимое значение")
+  }
+
+  if (arrayOfCharacters.length > 8) {
+    return console.log("Ошибка: Пин-код не может превышать болле 8 символов")
+  }
+
+
+  arrayOfCharacters.forEach((num) => {
+    if (typeof num !== "number") {
+      Number(num)
+    }
+    result.push(...POINTS[num])
+  })
+
+  return result.join(", ")
+}
